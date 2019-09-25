e5ae69822406744a5d6f30c5ff744cac7cbca97e
刘奕群 <>
2019-09-25 22:44:49 +0800

    属性动画
    1. animator资源文件夹
    2. (AnimatorSet) AnimatorInflater.loadAnimator
    3. setTarget

--- MyTimer/app/src/main/java/com/lyq/mytimer/ui/DebutActivity.java
+++ MyTimer/app/src/main/java/com/lyq/mytimer/ui/DebutActivity.java
@@ -1,9 +1,14 @@
 package com.lyq.mytimer.ui;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.lyq.mytimer.R;
@@ -24,5 +29,17 @@ public class DebutActivity extends BaseActivity {
 
 		TextView textView = findViewById(R.id.tv_content);
 		textView.setBackground(new DebutDrawable());
+
+		ImageView imageView = findViewById(R.id.img);
+		final AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.anim_debut);
+		set.setTarget(imageView);
+		set.start();
+
+		imageView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				set.start();
+			}
+		});
 	}
 }
