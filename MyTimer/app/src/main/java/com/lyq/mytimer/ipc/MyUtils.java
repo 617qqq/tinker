362bd131aca4a017b0f6d6205734b006ae6a4d12
刘奕群 <>
2019-10-09 08:54:28 +0800

    IPC:soket_tcp

--- MyTimer/app/src/main/java/com/lyq/mytimer/ipc/MyUtils.java
+++ MyTimer/app/src/main/java/com/lyq/mytimer/ipc/MyUtils.java
@@ -1,24 +1,42 @@
 package com.lyq.mytimer.ipc;
 
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 
 public class MyUtils {
 
-	public static void close(OutputStream stream) {
+	public static String getProcessName(Context cxt, int pid) {
+		ActivityManager am = (ActivityManager) cxt
+				.getSystemService(Context.ACTIVITY_SERVICE);
+		List<ActivityManager.RunningAppProcessInfo> runningApps = am.getRunningAppProcesses();
+		if (runningApps == null) {
+			return null;
+		}
+		for (ActivityManager.RunningAppProcessInfo procInfo : runningApps) {
+			if (procInfo.pid == pid) {
+				return procInfo.processName;
+			}
+		}
+		return null;
+	}
+
+	public static void close(Closeable closeable) {
 		try {
-			stream.close();
+			if (closeable != null) {
+				closeable.close();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void close(InputStream stream) {
-		try {
-			stream.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public static void executeInThread(Runnable runnable) {
+		new Thread(runnable).start();
 	}
 }
